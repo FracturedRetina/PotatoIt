86cbe8e5b78a1a86ce98d7e4ad1966a558fda63a
FracturedRetina <eshimoniak@gmail.com>
2014-02-04 17:03:19 -0800

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
 	chrome.storage.sync.get("enabled", function(items) {
-		if (items["enabled"] == "undefined") {
-			items["enabled"] = true;
-			
-			var setting = {};
-			setting["enabled"] = true;
-			chrome.storage.sync.set(setting, function() {});
-		}
-		
-		if (items["enabled"]) {
+		if (items["enabled"] != false) {
 			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
 		}
 	});
