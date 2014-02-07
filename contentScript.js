0277af7ec4f25a059ccdb5cbe2d01a6d1babcaa9
FracturedRetina <admin@krakendev.net>
2014-02-06 22:02:44 -0800

    Updated to extension Version 1.4

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-	chrome.storage.sync.get("enabled", function(items) {
-		if (items["enabled"] != false) {
+	chrome.storage.sync.get("level", function(items) {
+		console.log("Potato Level: " + items["level"]);
+		if (items["level"] == 0) {
+			return;
+		} else if (items["level"] == 1) {
+			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
+		} else if (items["level"] == 2) {
+			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
+			$('div').each(function(index) {
+				if ($(this).css('background-image') != 'auto') {
+					$(this).css('background-image', "url(" + chrome.extension.getURL("res/potato.png") + ")");
+					if ($(this).css('background-size') == 'auto') {
+						$(this).css('background-size', "100%");
+					}
+				}
+			});
+		} else if (items["level"] == 3) {
+			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
+			$('*').each(function(index) {
+				if ($(this).css('background-image') != 'auto') {
+					$(this).css('background-image', "url(" + chrome.extension.getURL("res/potato.png") + ")");
+					if ($(this).css('background-size') == 'auto') {
+						$(this).css('background-size', "100%");
+					}
+				}
+			});
+		} else {
 			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
 		}
 	});
--- options.js
+++ options.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
-	chrome.storage.sync.get("enabled", function(items) {
-		if (items["enabled"] == false) {
-			$('#enabled').attr("checked", false);
-		} else {
-			$('#enabled').attr("checked", true);
-		}
+	// chrome.storage.sync.get("enabled", function(items) {
+	// 	if (items["enabled"] == false) {
+	// 		$('#enabled').attr("checked", false);
+	// 	} else {
+	// 		$('#enabled').attr("checked", true);
+	// 	}
+	// });
+	
+	//Set up document
+	chrome.storage.sync.get("level", function(items) {
+		$('#level').val(items["level"]);
 	});
-
+	
+	
+	//Set button listener
 	$('#save').click(function() {
 		var setting = {};
-		setting["enabled"] = $('#enabled').is(":checked");
+		setting["level"] = $('#level').val();
 		
 		chrome.storage.sync.set(setting,
 			function() {
-				console.log("\"enabled\": " + setting);
+				console.log("\"level\": " + setting);
 			}
 		);
 
 		//Check if settings were properly saved
-		chrome.storage.sync.get("enabled", function(items) {
-			if (setting["enabled"] === items["enabled"]) {
+		chrome.storage.sync.get("level", function(items) {
+			if (setting["level"] === items["level"]) {
 				alert("Settings saved successfully.");
 			} else {
 				alert("Problem saving settings.");
