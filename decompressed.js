a35de4e22a12a60749d81014fdd8fba3eab0f72f
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-05 10:12:27 -0800

    Update decompressed.js

--- decompressed.js
+++ decompressed.js
@@ -1,32 +1,39 @@
+//Load jQuery
+var js = document.createElement('script');
+js.src = "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js";
+document.head.appendChild(js);
+
+
+//Starchy scripts
 $(document).ready(function() {
-	chrome.storage.sync.get("level", function(items) {
-		console.log("Potato Level: " + items["level"]);
-		if (items["level"] == 0) {
-			return;
-		} else if (items["level"] == 1) {
-			$('img[src]').attr('src',"http://www.howbigismypotato.com/potato.jpg"));
-		} else if (items["level"] == 2) {
-			$('img[src]').attr('src', "http://www.howbigismypotato.com/potato.jpg"));_____
-			$('div').each(function(index) {
-				if ($(this).css('background-image') != 'auto') {
-					$(this).css('background-image', "url(" + chrome.extension.getURL("res/potato.png") + ")");
-					if ($(this).css('background-size') == 'auto') {
-						$(this).css('background-size', "100%");
-					}
+	var level = 2;
+	
+	console.log("Potato Level: " + level);
+	if (level == 0) {
+		return;
+	} else if (level == 1) {
+		$('img[src]').attr('src', "http://www.howbigismypotato.com/potato.jpg"));
+	} else if (level == 2) {
+		$('img[src]').attr('src', "http://www.howbigismypotato.com/potato.jpg"));
+		$('div').each(function(index) {
+			if ($(this).css('background-image') != 'auto') {
+				$(this).css('background-image', "url(http://www.howbigismypotato.com/potato.jpg)");
+				if ($(this).css('background-size') == 'auto') {
+					$(this).css('background-size', "100%");
 				}
-			});
-		} else if (items["level"] == 3) {
-			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
-			$('*').each(function(index) {
-				if ($(this).css('background-image') != 'auto') {
-					$(this).css('background-image', "url(" + chrome.extension.getURL("res/potato.png") + ")");
-					if ($(this).css('background-size') == 'auto') {
-						$(this).css('background-size', "100%");
-					}
+			}
+		});
+	} else if (level == 3) {
+		$('img[src]').attr('src', "http://www.howbigismypotato.com/potato.jpg"):
+		$('*').each(function(index) {
+			if ($(this).css('background-image') != 'auto') {
+				$(this).css('background-image', "url(http://www.howbigismypotato.com/potato.jpg)");
+				if ($(this).css('background-size') == 'auto') {
+					$(this).css('background-size', "100%");
 				}
-			});
-		} else {
-			$('img[src]').attr('src', chrome.extension.getURL("res/potato.png"));
-		}
-	});
+			}
+		});
+	} else {
+		$('img[src]').attr('src', "http://www.howbigismypotato.com/potato.jpg");
+	}
 });
